5873fd125457b6747b50a8dd01f548f794d856df
Martin Czygan <martin.czygan@gmail.com>
2021-06-11 15:22:03 +0200

    make sure it's not JSON

--- cmd/gdate/main.go
+++ cmd/gdate/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"log"
 	"time"
 
 	"github.com/fatih/color"
 	"github.com/miku/gdate"
+	"go4.org/strutil"
 )
 
 func main() {
 	t := time.Now().Format(time.RFC3339)
+	if strutil.IsPlausibleJSON(t) {
+		log.Println("that's weird")
+	}
 	color.Green(gdate.Preamble() + t)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/miku/gdate
 
 go 1.16
 
-require github.com/fatih/color v1.12.0
+require (
+	github.com/fatih/color v1.12.0
+	go4.org v0.0.0-20201209231011-d4a079459e60 // indirect
+)
